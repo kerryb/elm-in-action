038b6643cd928227496172f70c61f3077206cd47
Kerry Buckley <kerryjbuckley@gmail.com>
2017-07-10 15:38:45 +0100

    Display thumbs from model

--- photo-groove/PhotoGroove.elm
+++ photo-groove/PhotoGroove.elm
@@ -5,12 +5,17 @@ import Html.Attributes exposing (..)
 view model =
   div [ class "content" ]
       [ h1 [] [ text "Photo Groove" ]
-      , div [ id "thumbnails" ]
-        [ img [ src "img/thumb-1.jpg" ] []
-        , img [ src "img/thumb-2.jpg" ] []
-        , img [ src "img/thumb-3.jpg" ] []
-        ]
+      , div [ id "thumbnails" ] (List.map viewThumbnail model)
       ]
 
+viewThumbnail thumbnail = 
+  img [ src ("img/thumb/" ++ thumbnail.url) ] []
+
+initialModel = 
+  [ { url = "1.jpg" }
+  , { url = "2.jpg" }
+  , { url = "3.jpg" }
+  ]
+
 main = 
-  view "no model yet"
+  view initialModel
