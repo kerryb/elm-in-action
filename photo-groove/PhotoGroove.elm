8a427ee31f7518ffc1cc581fb6980912f53c50e8
Kerry Buckley <kerryjbuckley@gmail.com>
2017-07-10 16:12:52 +0100

    Show large image and highlight selected thumbnail

--- photo-groove/PhotoGroove.elm
+++ photo-groove/PhotoGroove.elm
@@ -5,17 +5,28 @@ import Html.Attributes exposing (..)
 view model =
   div [ class "content" ]
       [ h1 [] [ text "Photo Groove" ]
-      , div [ id "thumbnails" ] (List.map viewThumbnail model)
+      , div [ id "thumbnails" ]
+            (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+      , img [ src ("img/full/" ++ model.selectedUrl)
+            , class "large"
+            ]
+            []
       ]
 
-viewThumbnail thumbnail = 
-  img [ src ("img/thumb/" ++ thumbnail.url) ] []
+viewThumbnail selectedUrl photo =
+    img [ src ("img/thumb/" ++ photo.url)
+          , classList [ ( "selected", selectedUrl == photo.url ) ]
+          ]
+          []
 
-initialModel = 
-  [ { url = "1.jpg" }
-  , { url = "2.jpg" }
-  , { url = "3.jpg" }
-  ]
+initialModel =
+  { photos =
+    [ { url = "1.jpg" }
+    , { url = "2.jpg" }
+    , { url = "3.jpg" }
+    ]
+    , selectedUrl = "1.jpg"
+  }
 
-main = 
+main =
   view initialModel
